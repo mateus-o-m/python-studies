b45cd0a96b6e4950f08956954605260d6c135fd0
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-02-26 10:11:54 -0300

    Adding market.py structure

---
+++ mini-projects/marketInventary/market.py
@@ -0,0 +1,22 @@
+# while (1)
+print ("MARKET STOCK")
+print ("Digite o número correspondente á ação desejada:")
+print ("1. Adcionar novo produto ao estoque.")
+print ("2. Exibir total de produtos cadastrados.")
+print ("0. Sair do programa")
+
+def addProduct (dicProduct, listStock):
+	dicProduct["id"] = 5
+	dicProduct["name"] = str (input("Digite o nome do produto: "))
+	dicProduct["price"] = float (input("Digite o valor de {dicProduct['name']}:"))
+	dicProduct["amount"] = int (input("DIgite a quantidade de {dicProduct['name']}:"))
+	listStock.append(dicProduct)
+
+# main()
+products = {"name": "nome", "id": 0, "price": 0.0, "amount": 0}
+marketStock = []
+# print (type(products))
+
+addProduct (products, marketStock)
+
+print (marketStock)
