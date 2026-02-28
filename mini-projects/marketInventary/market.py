2ce907eb576627330502d94d71bc5a43711113e2
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-02-28 14:19:30 -0300

    Adding a manu function and a better addProduct function structure

--- mini-projects/marketInventary/market.py
+++ mini-projects/marketInventary/market.py
@@ -1,18 +1,35 @@
-# while (1)
-print ("MARKET STOCK")
-print ("Digite o número correspondente á ação desejada:")
-print ("1. Adcionar novo produto ao estoque.")
-print ("2. Exibir total de produtos cadastrados.")
-print ("0. Sair do programa")
+def menu (listStock):
+	id = 1
+	while (1):
+		print ("1. Adcionar novo produto ao estoque.")
+		print ("2. Exibir total de produtos cadastrados.")
+		print ("0. Sair do programa")
+		menuOption = int (input("Digite o número correspondente á ação desejada: "))
+		if menuOption == 1:
+			addProduct (listStock, id)
+			id += 1
+		elif menuOption == 2:
+			countProduct (listStock)
+		elif menuOption == 0:
+			break
 
-def addProduct (dicProduct, listStock):
-	dicProduct["id"] = 5
+def addProduct (listStock, idProduct):
+	dicProduct = {}
 	dicProduct["name"] = str (input("Digite o nome do produto: "))
-	dicProduct["price"] = float (input("Digite o valor de {dicProduct['name']}:"))
-	dicProduct["amount"] = int (input("DIgite a quantidade de {dicProduct['name']}:"))
-	listStock.append(dicProduct)
+	for product in listStock:
+		if product["name"] == dicProduct["name"]:
+			print ("Produto já cadastrado. Utilize os campos abaixo para editar o produto.\nSe não forem preenchidos, o valor atual será matido")
+			dicProduct["price"] = float (input(f"Atualizar o valor de {dicProduct['name']}, valor atual {dicProduct['price']}: "))
+			dicProduct["amount"] += int (input(f"Atualizar a quantidade de {dicProduct['name']}, quantidade atual {dicProduct['amount']}:"))
+			listStock.append (dicProduct)
+		return
+	dicProduct["id"] = idProduct
+	dicProduct["price"] = float (input(f"Digite o valor de {dicProduct['name']}: "))
+	dicProduct["amount"] = int (input(f"DIgite a quantidade de {dicProduct['name']}: "))
+	listStock.append (dicProduct)
 
 def countProduct (listStock):
+	countVar = 0
 	productAmount = []
 	for product in listStock:
 		countVar += product["amount"]
@@ -21,12 +38,11 @@ def countProduct (listStock):
 	return productAmount
 
 # main()
-products = {"name": "nome", "id": 0, "price": 0.0, "amount": 0}
 marketStock = []
-# print (type(products))
 
-addProduct (products, marketStock)
-addProduct (products, marketStock)
-print (countProduct (marketStock))
+print ("========== MARKET STOCK ==========")
+menu (products, marketStock)
 
-print (marketStock[0])
+print (marketStock)
+
+# print (marketStock[0])
