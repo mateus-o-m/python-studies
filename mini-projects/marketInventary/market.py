d75d341f4b21e8c23a1dca93ada6469fc90d28e9
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-02-28 15:18:30 -0300

    Adding verifyInput function. Reestructuring code, work in progress

--- mini-projects/marketInventary/market.py
+++ mini-projects/marketInventary/market.py
@@ -1,35 +1,47 @@
+def verifyInput (dataType, messageString): # verifica input de diferentes tipos numéricos, com uma mensagem personalizada
+	while (1):
+		value = dataType (input(messageString))
+		if value == ""
+			return
+		try:
+			value > 0
+			return value
+		except ValueError:
+			print ("Entrada inválida. Digite um número válido.")
+
 def menu (listStock):
 	id = 1
+	options = """
+		1. Adcionar novo produto ao estoque.
+		2. Exibir total de produtos cadastrados.
+		0. Sair do programa
+		"""
 	while (1):
-		print ("1. Adcionar novo produto ao estoque.")
-		print ("2. Exibir total de produtos cadastrados.")
-		print ("0. Sair do programa")
-		menuOption = int (input("Digite o número correspondente á ação desejada: "))
+		print (options)
+		menuOption = verifyInput (int, "Digite o número correspondente á ação desejada: ")
 		if menuOption == 1:
 			addProduct (listStock, id)
 			id += 1
 		elif menuOption == 2:
-			countProduct (listStock)
+			print (countProduct(listStock)
 		elif menuOption == 0:
 			break
 
 def addProduct (listStock, idProduct):
-	dicProduct = {}
-	dicProduct["name"] = str (input("Digite o nome do produto: "))
+	name = str (input("Digite o nome do produto: ").lower())
 	for product in listStock:
 		if product["name"] == dicProduct["name"]:
-			print ("Produto já cadastrado. Utilize os campos abaixo para editar o produto.\nSe não forem preenchidos, o valor atual será matido")
-			dicProduct["price"] = float (input(f"Atualizar o valor de {dicProduct['name']}, valor atual {dicProduct['price']}: "))
-			dicProduct["amount"] += int (input(f"Atualizar a quantidade de {dicProduct['name']}, quantidade atual {dicProduct['amount']}:"))
-			listStock.append (dicProduct)
+			print (f"{dicProduct['name']} já cadastrado")
+			dicProduct["price"] = verifyInput (float, f"Preço atual {dicProduct['price']}, digite novo preço: ")
+			dicProduct["amount"] = verifyInput (int, f"Quantidade atual {dicProduct['amount']}, digite nova quantidade")
 		return
-	dicProduct["id"] = idProduct
-	dicProduct["price"] = float (input(f"Digite o valor de {dicProduct['name']}: "))
-	dicProduct["amount"] = int (input(f"DIgite a quantidade de {dicProduct['name']}: "))
+	dicProduct = {"name": name, "id": idProduct,
+		"price": verifyInput (float, f"Digite o valor de {dicProduct['name']}: "),
+		"amount": verifyInput (int, f"DIgite a quantidade de {dicProduct['name']}: ")}
 	listStock.append (dicProduct)
 
 def countProduct (listStock):
-	countVar = 0
+	countVar = ;0
 	productAmount = []
 	for product in listStock:
 		countVar += product["amount"]
@@ -39,10 +51,5 @@ def countProduct (listStock):
 
 # main()
 marketStock = []
-
 print ("========== MARKET STOCK ==========")
-menu (products, marketStock)
-
-print (marketStock)
-
-# print (marketStock[0])
+menu (marketStock)
